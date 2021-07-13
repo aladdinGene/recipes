a3e18c09f47097df0464444605db5b035c7231c2
NikitaRaik <79733831+NikitaRaik@users.noreply.github.com>
2021-07-13 01:14:11 -0700

    w3r3

--- recipes/settings.py
+++ recipes/settings.py
@@ -141,10 +141,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
